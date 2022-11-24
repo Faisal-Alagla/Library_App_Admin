a5103b3ef9849aa15d98858035c357e4a29a47c4
Faisal <alagla.faisal@gmail.com>
2022-11-24 23:46:33 +0300

    cleaning

--- Pages/profile.php
+++ Pages/profile.php
@@ -52,113 +52,117 @@ $user = $auth->getUser($_SESSION['verified_user_id']);
                                     if (isset($_GET['edit'])) {
                                     ?>
 
-                            <form method="POST" action="../db/profile_management.php">
-                                <input type="hidden" name="id" value="<?php echo $user->uid ?>" />
-                                <input type="text" id="dName" name="dName"
-                                    class="form-control form-control-lg shadow-lg "
-                                    style="border-radius: 15px; padding-right: 40px;"
-                                    value="<?php echo $user->displayName ?>" required />
-                                <i class="bi bi-person-fill"></i>
+                                    <form method="POST" action="../db/profile_management.php">
+                                        <input type="hidden" name="id" value="<?php echo $user->uid ?>" />
+                                        <input type="text" id="dName" name="dName"
+                                            class="form-control form-control-lg shadow-lg "
+                                            style="border-radius: 15px; padding-right: 40px;"
+                                            value="<?php echo $user->displayName ?>" required />
+                                        <i class="bi bi-person-fill"></i>
 
-                                <div class="d-flex flex-row justify-content-evenly align-items-center mt-2">
-                                    <button class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
-                                        style="background-color: #212B5E; border-radius: 15px" type="update_dName"
-                                        name="update_dName">
-                                        Update
-                                    </button>
+                                        <div class="d-flex flex-row justify-content-evenly align-items-center mt-2">
+                                            <button class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
+                                                style="background-color: #212B5E; border-radius: 15px"
+                                                type="update_dName" name="update_dName">
+                                                Update
+                                            </button>
 
-                                    <a class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
-                                        style="background-color: #98030e; border-radius: 15px" href="profile.php">
-                                        Cancel
-                                    </a>
-                                </div>
-                            </form>
+                                            <a class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
+                                                style="background-color: #98030e; border-radius: 15px"
+                                                href="profile.php">
+                                                Cancel
+                                            </a>
+                                        </div>
+                                    </form>
 
-                            <?php
+                                    <?php
                                     }
                                     //normal display
                                     else {
                                     ?>
 
-                            <div class="edit">
-                                <hr class="mt-0 mb-1">
-                                <h3><?php echo $user->displayName ?></h3>
-                                <a href="profile.php?edit=edit">
-                                    <i class="bi bi-pencil-square"></i>
-                                </a>
-                            </div>
+                                    <div class="edit">
+                                        <hr class="mt-0 mb-1">
+                                        <h3><?php echo $user->displayName ?></h3>
+                                        <a href="profile.php?edit=edit">
+                                            <i class="bi bi-pencil-square"></i>
+                                        </a>
+                                    </div>
 
-                            <?php
+                                    <?php
 
                                     }
                                     ?>
 
-                        </div>
-                    </div>
-                    <!-- Display name end -->
+                                </div>
+                            </div>
+                            <!-- Display name end -->
 
-                    <!-- Change password start -->
-                    <?php
+                            <!-- Change password start -->
+                            <?php
                             //when trying to change password
                             if (isset($_GET['changepwd'])) {
                             ?>
-                    <div class="col-sm-12 d-flex justify-content-center mb-4 text-start">
-                        <div class="mb-3 mt-3 text-start input" style="min-width: 50%;">
-                            <form method="POST" action="../db/profile_management.php">
-                                <input type="hidden" name="id" value="<?php echo $user->uid ?>" />
+                            <div class="col-sm-12 d-flex justify-content-center mb-4 text-start">
+                                <div class="mb-3 mt-3 text-start input" style="min-width: 50%;">
+                                    <form method="POST" action="../db/profile_management.php">
+                                        <input type="hidden" name="id" value="<?php echo $user->uid ?>" />
 
-                                <label class="form-label pt-2" for="password" style="color:#212B5E;">Password</label>
-                                <input type="password" id="password" name="password"
-                                    class="form-control form-control-lg shadow-lg"
-                                    style="border-radius: 15px; padding-inline: 15px;" minlength="8" required />
+                                        <label class="form-label pt-2" for="password"
+                                            style="color:#212B5E;">Password</label>
+                                        <input type="password" id="password" name="password"
+                                            class="form-control form-control-lg shadow-lg"
+                                            style="border-radius: 15px; padding-inline: 15px;" minlength="8" required />
 
-                                <label class="form-label pt-2" for="re-password"
-                                    style="color:#212B5E;">Re-password</label>
-                                <input type="password" id="re-password" name="re-password"
-                                    class="form-control form-control-lg shadow-lg"
-                                    style="border-radius: 15px; padding-inline: 15px;" minlength="8" required />
+                                        <label class="form-label pt-2" for="re-password"
+                                            style="color:#212B5E;">Re-password</label>
+                                        <input type="password" id="re-password" name="re-password"
+                                            class="form-control form-control-lg shadow-lg"
+                                            style="border-radius: 15px; padding-inline: 15px;" minlength="8" required />
 
-                                <div class="d-flex flex-row justify-content-evenly align-items-center mt-2 pt-2">
-                                    <button class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
-                                        style="background-color: #212B5E; border-radius: 15px" type="update_pwd"
-                                        name="update_pwd">
-                                        Update
-                                    </button>
+                                        <div
+                                            class="d-flex flex-row justify-content-evenly align-items-center mt-2 pt-2">
+                                            <button class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
+                                                style="background-color: #212B5E; border-radius: 15px" type="update_pwd"
+                                                name="update_pwd">
+                                                Update
+                                            </button>
 
-                                    <a class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
-                                        style="background-color: #98030e; border-radius: 15px" href="profile.php">
-                                        Cancel
-                                    </a>
+                                            <a class="btn btn-lg btn-block text-white w-50 display-2 mx-1 "
+                                                style="background-color: #98030e; border-radius: 15px"
+                                                href="profile.php">
+                                                Cancel
+                                            </a>
+                                        </div>
+                                    </form>
                                 </div>
-                            </form>
-                        </div>
-                    </div>
+                            </div>
 
-                    <?php
+                            <?php
                             }
                             //normal display
                             else {
                             ?>
 
-                    <div class="pt-5">
-                        <a class="btn btn-lg btn-block text-white display-2 mx-1 "
-                            style="background-color: #212B5E; border-radius: 15px"
-                            href="profile.php?changepwd=changepwd">
-                            Change Password
-                        </a>
-                    </div>
-                    <!-- Change password end -->
+                            <div class="pt-5">
+                                <a class="btn btn-lg btn-block text-white display-2 mx-1 "
+                                    style="background-color: #212B5E; border-radius: 15px"
+                                    href="profile.php?changepwd=changepwd">
+                                    Change Password
+                                </a>
+                            </div>
+                            <!-- Change password end -->
 
-                    <?php
+                            <?php
                             }
                             ?>
 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
 </div>
 
 <?php
