5bb9b4caf8dd10c1a9435481e252eed42b322969
Faisal <alagla.faisal@gmail.com>
2022-11-27 13:10:42 +0300

    update book checks for existing objects done

--- db/edit_book_action.php
+++ db/edit_book_action.php
@@ -1,72 +1,81 @@
 <?php
 include("config.php");
+include('functions.php');
 
 if (isset($_POST['update_book'])) {
     $key = $_POST['key'];
     $isbn = $_POST['isbn'];
-    $title = $_POST['title'];
-    $author = $_POST['author'];
-    $category = $_POST['category'];
-    $image = $_FILES['image']['name'];
-    $date = $_POST['date'];
-    $summary = $_POST['summary'];
 
-    $ref_table = "books/" . $key;
-
-    if ($image != Null) {
-        //new image is uploaded
-        $image_name = $database->getReference($ref_table)->getValue()['image'];
-        if ($image_name > 0) {
-            //deleting the old img
-            // unlink('../images/book_images/' . $image_name);
-            $bucket->object('images/'.$image_name)->delete();
-        }
-
-        //uploading the new image
-        $image_name = $isbn . $image;
-        // move_uploaded_file($_FILES['image']['tmp_name'], '../images/book_images/' . $image_name);
-        $file_contents = file_get_contents($_FILES['image']['tmp_name']);
-        $bucket->upload($file_contents, ['name' => 'images/' . $image_name]);
+    $book_exists = object_exists($key, $isbn, $database, 'books', 'isbn');
+    if ($book_exists) {
+        //if a different book with the same isbn exists
+        $_SESSION['book_key'] = $key;
+        $_SESSION['book_updated_flag'] = false;
+        $_SESSION['book_updated'] = "A book with the same isbn exists, please check if the book already exists in the books table!";
     } else {
-        //no uploaded image
-        $image_name = $database->getReference($ref_table)->getValue()['image'];
+        $title = $_POST['title'];
+        $author = $_POST['author'];
+        $category = $_POST['category'];
+        $image = $_FILES['image']['name'];
+        $date = $_POST['date'];
+        $summary = $_POST['summary'];
 
-        //if there was an image in DB
-        if ($image_name > 0) {
-            $no_isbn_name = substr($image_name, 10);
-            $new_image_name = $isbn . $no_isbn_name;
-
-            // rename('../images/book_images/' . $image_name, '../images/book_images/' . $new_image_name);
-            if($image_name != $new_image_name) {
-                //if image name / isbn changed -> change image name in db storage
-                $object = $bucket->object('images/'.$image_name);
-                $object->copy($bucket, ['name' => 'images/'.$new_image_name]);
-                $object->delete();
+        $ref_table = "books/" . $key;
+        if ($image != Null) {
+            //new image is uploaded
+            $image_name = $database->getReference($ref_table)->getValue()['image'];
+            if ($image_name > 0) {
+                //deleting the old img
+                // unlink('../images/book_images/' . $image_name);
+                $bucket->object('images/' . $image_name)->delete();
             }
 
-            $image_name = $new_image_name;
+            //uploading the new image
+            $image_name = $isbn . $image;
+            // move_uploaded_file($_FILES['image']['tmp_name'], '../images/book_images/' . $image_name);
+            $file_contents = file_get_contents($_FILES['image']['tmp_name']);
+            $bucket->upload($file_contents, ['name' => 'images/' . $image_name]);
+        } else {
+            //no uploaded image
+            $image_name = $database->getReference($ref_table)->getValue()['image'];
+
+            //if there was an image in DB
+            if ($image_name > 0) {
+                $no_isbn_name = substr($image_name, 10);
+                $new_image_name = $isbn . $no_isbn_name;
+
+                // rename('../images/book_images/' . $image_name, '../images/book_images/' . $new_image_name);
+                if ($image_name != $new_image_name) {
+                    //if image name / isbn changed -> change image name in db storage
+                    $object = $bucket->object('images/' . $image_name);
+                    $object->copy($bucket, ['name' => 'images/' . $new_image_name]);
+                    $object->delete();
+                }
+
+                $image_name = $new_image_name;
+            }
         }
-    }
 
-    $updateData = [
-        'isbn' => $isbn,
-        'title' => $title,
-        'author' => $author,
-        'category' => $category,
-        'image' => $image_name,
-        'date' => $date,
-        'summary' => $summary,
-    ];
+        $updateData = [
+            'isbn' => $isbn,
+            'title' => $title,
+            'author' => $author,
+            'category' => $category,
+            'image' => $image_name,
+            'date' => $date,
+            'summary' => $summary,
+        ];
 
-    $update_query_result =  $database->getReference($ref_table)->update($updateData);
+        $update_query_result =  $database->getReference($ref_table)->update($updateData);
 
-    $_SESSION['book_key'] = $key;
-    if ($update_query_result) {
-        $_SESSION['book_updated_flag'] = true;
-        $_SESSION['book_updated'] = "Book updated successfully!";
-    } else {
-        $_SESSION['book_updated_flag'] = false;
-        $_SESSION['book_updated'] = "Something went wrong, please try again later!";
+        $_SESSION['book_key'] = $key;
+        if ($update_query_result) {
+            $_SESSION['book_updated_flag'] = true;
+            $_SESSION['book_updated'] = "Book updated successfully!";
+        } else {
+            $_SESSION['book_updated_flag'] = false;
+            $_SESSION['book_updated'] = "Something went wrong, please try again later!";
+        }
     }
 } else {
     $_SESSION['book_key'] = false;
--- db/functions.php
+++ db/functions.php
@@ -1,18 +1,41 @@
 <?php
 /*
-item_exists(): function to check if an item already exists in the database
-parameters:   $cat: the category to check for
+item_exists(): function to check if an object with the same attribute already exists in the database (using attributes, NOT keys!)
+               exmaple: checking if a book isbn already exists among the books in the DB
+parameters:   $attribute: the attribute to check for
               $db: to pass $database for querying
               $ref_table: the database tale to look in
               $col_name: column name in the ref_table
-returns true if the passed item already exists, false otherwise
+returns true if the passed attribute already exists, false otherwise
 */
-function item_exists($item, $db, $ref_table, $col_name)
+function item_exists($attribute, $db, $ref_table, $col_name)
 {
     $fetch_item = $db->getReference($ref_table)->getValue();
 
     foreach ($fetch_item as $row) {
-        if ($row[$col_name] == $item) {
+        if ($row[$col_name] == $attribute) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+object_exists(): function to check if an object with the same attribute, but not the key already exists in the database
+                 example: checking if a book with the same ISBN but not the same key exists in the DB
+parameters:   $obj_key: key of the obj to compare with
+              $attribute: the attribute to check for
+              $db: to pass $database for querying
+              $ref_table: the database tale to look in
+              $col_name: column name in the ref_table
+returns true only if the passed attribute matches the attribute of another object with a different key, false otherwise
+*/
+function object_exists($obj_key, $attribute, $db, $ref_table, $col_name)
+{
+    $fetch_item = $db->getReference($ref_table)->getValue();
+
+    foreach ($fetch_item as $fetched_key => $row) {
+        if (($row[$col_name] == $attribute) && !($fetched_key == $obj_key)) {
             return true;
         }
     }
