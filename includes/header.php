4a9f0483bc1279a78c9650f9f339639ca9c6dfd3
Faisal <alagla.faisal@gmail.com>
2022-12-01 09:01:54 +0300

    Navbar Adminpanel home link

--- includes/header.php
+++ includes/header.php
@@ -75,7 +75,7 @@ if (($cur_page == "add_book.php") || ($cur_page == "book_list.php") || ($cur_pag
 <body style="background-color: #1b1b1b">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand px-3" href="#">Admin Panel</a>
+      <a class="navbar-brand px-3" href="home.php">Admin Panel</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
